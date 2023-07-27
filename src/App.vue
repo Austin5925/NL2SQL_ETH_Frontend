5e1eefcbbe07020d18c9550efcfb0b2343a9619d
Austin5925 <z1026804039@163.com>
2023-07-27 16:23:01 +0800

    v0.2 Multi-chat added

--- src/App.vue
+++ src/App.vue
@@ -10,26 +10,60 @@
         <h1 class="h1">ETH问数成章</h1>
       </div>
       <input
-        v-model="input"
+        v-model="inputQuery.inputValue"
         class="form-control mb-3"
         style="max-width: 600px; margin: auto"
       />
-      <button id="submit" @click="query" class="btn btn-primary">提交</button>
+      <div class="flex-line">
+        <input type="checkbox" id="checkbox1" v-model="isChatMode" />
+        <label for="checkbox1" style="margin-left: 8px; margin-right: 15px"
+          >聊天模式</label
+        >
+        <button id="submit" @click="submit" class="btn btn-primary">
+          {{ isChatMode ? "聊天" : "查询" }}
+        </button>
+        <button id="refresh" @click="refresh" class="btn btn-primary">
+          刷新
+        </button>
+        <button
+          id="query"
+          @click="query"
+          v-if="isClear && isChatMode"
+          class="btn btn-primary"
+        >
+          查询
+        </button>
+      </div>
       <p class="mt-5 mb-3">
         连接状态：<span id="status">{{ status }}</span>
       </p>
-      <p class="mb-3">
+      <p class="mb-3" v-if="!isChatMode">
         GPT回复：<span id="message">{{ message }}</span>
       </p>
+      <div v-if="isChatMode" class="chat-box">
+        <div
+          v-for="item in messages"
+          :key="item.id"
+          :class="`message ${item.role}`"
+        >
+          <div class="message-content">
+            {{ item.content }}
+          </div>
+        </div>
+      </div>
       <p class="mt-5 mb-3">
-        <echarts-component :data="echartsRes"></echarts-component>
+        <echarts-component
+          ref="EchartsComponent"
+          :data="echartsRes"
+          @append-table="drawTable"
+        ></echarts-component>
       </p>
     </div>
   </div>
 </template>
 
 <script>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, reactive } from "vue";
 import axios from "axios";
 import * as echarts from "echarts/core";
 import { CanvasRenderer } from "echarts/renderers";
@@ -44,15 +78,120 @@ export default {
   props: {
     // todo
   },
+  setup() {
+    const inputQuery = reactive({
+      inputValue: "",
+    });
+
+    return { inputQuery };
+  },
   data() {
     return {
-      input: "",
+      // input: "",
       message: "",
       status: "未连接",
       echartsRes: null,
+      isChatMode: true,
+      conversation: null,
+      messages: [],
+      isClear: false,
+      tempLength: 0,
     };
   },
   methods: {
+    submit() {
+      if (this.isChatMode) {
+        this.chat();
+      } else {
+        this.query();
+      }
+    },
+    refresh() {
+      window.location.reload();
+      axios
+        .get("http://localhost:4536/refresh")
+        .then((response) => {
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    chat() {
+      let messageReceived = false;
+      const data = { input: this.inputQuery.inputValue };
+      const url = `http://localhost:4536/multi-round-chat?input=${encodeURIComponent(
+        this.inputQuery.inputValue
+      )}`;
+
+      this.messages.push({
+        id: this.tempLength++,
+        role: "user",
+        content: this.inputQuery.inputValue,
+      });
+
+      const eventSource = new EventSource(url, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      eventSource.onopen = () => {
+        this.status = "已连接";
+        this.messages.push({
+          id: this.tempLength++,
+          role: "assistant",
+          content: "",
+        });
+      };
+
+      eventSource.onmessage = (event) => {
+        messageReceived = true;
+        const data = JSON.parse(event.data);
+        this.messages[this.tempLength - 1].content = data.content;
+      };
+
+      eventSource.onerror = (event) => {
+        if (messageReceived) {
+          console.log("Connection closed by server.");
+          this.status = "连接已关闭";
+
+          //获取redis的查询数据
+          axios
+            .get("http://localhost:4536/multi-round-chat/result")
+            .then((response) => {
+              this.conversation = response.data["default"];
+              this.conversation.shift();
+              this.isClear =
+                parseInt(
+                  this.conversation[this.conversation.length - 1]["isClear"]
+                ) === 1;
+              this.conversation.pop();
+              console.log(this.conversation);
+
+              if (this.isClear === false) {
+              } else if (this.isClear === true) {
+                let regex = /您的问题已经足够清楚：(.*?)。/;
+                let match =
+                  this.messages[this.tempLength - 1].content.match(regex);
+                if (match && match[1]) {
+                  this.inputQuery.inputValue = match[1];
+                }
+              } else {
+                console.log("error");
+              }
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        } else {
+          console.log("Error:", event);
+          this.status = "连接错误";
+        }
+        eventSource.close();
+      };
+    },
     query() {
       let messageReceived = false;
       const data = { input: this.input };
@@ -85,8 +224,9 @@ export default {
           axios
             .get("http://localhost:4536/chat/result")
             .then((response) => {
-              console.log(response.data[0]);
-              this.echartsRes = response.data[0];
+              console.log(response.data);
+              this.echartsRes = response.data;
+              this.drawTable(response.data[0]);
             })
             .catch((error) => {
               console.log(error);
@@ -98,6 +238,11 @@ export default {
         eventSource.close();
       };
     },
+    drawTable(jsonArray) {
+      console.log("drawTable");
+      console.log(jsonArray);
+      this.$refs.echartsComponent.appendTable(jsonArray);
+    },
   },
   created() {},
   mounted() {},
@@ -106,14 +251,62 @@ export default {
 
 <style>
 body {
-  //background: linear-gradient(to right, #6a82fb, #fc5c7d 40%, #05dfd7);
-  color: #fff;
+  //background: linear-gradient(to right, #6a82fb, #fc5c7d 40%, #05dfd7); color: #fff;
+}
+
+.chat-box {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 100%;
+  height: auto;
+  overflow: auto;
+  border: 1px solid #eee;
+  background-color: #fafafa;
+  padding: 10px;
+  margin-bottom: 20px;
+}
+
+.message {
+  display: inline-block;
+  width: auto;
+  max-width: 80%;
+  padding: 10px;
+  margin: 10px 0;
+  border-radius: 10px;
+}
+
+.message.user {
+  margin-left: auto;
+  align-self: flex-end;
+  text-align: right;
+  background-color: #dcf8c6;
+}
+
+.message.assistant {
+  margin-right: auto;
+  align-self: flex-start;
+  text-align: left;
+  background-color: #ece5dd;
+}
+
+.message-content {
+  word-wrap: break-word;
+}
+
+.flex-line {
+}
+
+#refresh {
+  margin-left: 12px;
+  margin-right: 12px;
 }
 
 .h1 {
   position: relative;
   margin: 12px 0 15px;
 }
+
 .form-control {
 }
 
--- src/components/EchartsComponent.vue
+++ src/components/EchartsComponent.vue
@@ -1,16 +1,124 @@
 <script>
+// import { GridComponent, TooltipComponent } from "echarts/components";
+// import { CanvasRenderer } from "echarts/renderers";
+// import { BarChart } from "echarts/charts";
+// import * as echarts from "echarts/core";
+// import axios from "axios";
+// import { ref, onMounted } from "vue";
+// import EchartsComponent from "@/components/EchartsComponent.vue";
+
+import axios from "axios";
+
 export default {
   props: ["data"],
+  data() {
+    return {
+      hasTable: true,
+      headArray: [],
+    };
+  },
+  methods: {
+    appendTable: function (jsonArray) {
+      //todo，需要修改逻辑和语法
+      this.parseHead(jsonArray[0]);
+      let div = document.getElementById("table-result");
+      let table = document.createElement("table");
+      let thead = document.createElement("tr");
+      for (let count = 0; count < this.headArray.length; count++) {
+        let td = document.createElement("th");
+        td.innerHTML = this.headArray[count];
+        thead.appendChild(td);
+      }
+      table.appendChild(thead);
+      for (let tableRowNo = 0; tableRowNo < jsonArray.length; tableRowNo++) {
+        let tr = document.createElement("tr");
+        for (
+          let headCount = 0;
+          headCount < this.headArray.length;
+          headCount++
+        ) {
+          let cell = document.createElement("td");
+          cell.innerHTML = jsonArray[tableRowNo][this.headArray[headCount]];
+          tr.appendChild(cell);
+        }
+        table.appendChild(tr);
+      }
+      div.appendChild(table);
+      this.$emit("append-table", jsonArray);
+    },
+    parseHead: function (oneRow) {
+      for (let i in oneRow) {
+        this.headArray[this.headArray.length] = i;
+      }
+    },
+    //获取redis的查询数据
+    getRedisData() {
+      axios
+        .get("http://localhost:4536/redis")
+        .then((res) => {
+          console.log(res.data);
+          this.headArray = res.data.head;
+          this.hasTable = true;
+          this.echartsRes = res.data.data;
+          this.drawEcharts();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    //绘制echarts
+    drawEcharts() {
+      const myChart = echarts.init(document.getElementById("table-result"));
+      const option = {
+        tooltip: {
+          trigger: "axis",
+          axisPointer: {
+            type: "shadow",
+          },
+        },
+        legend: {
+          data: ["数量"],
+        },
+        grid: {
+          left: "3%",
+          right: "4%",
+          bottom: "3%",
+          containLabel: true,
+        },
+        xAxis: {
+          type: "value",
+          boundaryGap: [0, 0.01],
+        },
+        yAxis: {
+          type: "category",
+          data: this.headArray,
+        },
+        series: [
+          {
+            name: "数量",
+            type: "bar",
+            data: this.echartsRes,
+          },
+        ],
+      };
+      myChart.setOption(option);
+    },
+  },
 };
 </script>
 
 <template>
   <div>
-    <div class="mt-5 mb-3">
-      查询结果:
-      <p style="font-weight: bold">
+    <p class="mt-5 mb-3">
+      查询结果：
+      <span style="font-weight: bold">
         {{ data }}
-      </p>
+      </span>
+    </p>
+    <div v-show="hasTable">
+      <p class="mt-5 mb-3">数据图表：</p>
+      <!-- 数据展示 -->
+      <div id="table-result"></div>
     </div>
   </div>
 </template>
